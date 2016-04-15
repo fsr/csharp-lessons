b2169353544bcc09bffaa77af3ad12074664f853
Sascha Peukert <sascha.peukert@gmail.com>
2016-04-15 22:20:54 +0200

    Added section about architecture and changed small details

--- latex/slides/resources/01_grundlagen_1/helloworld.cs
+++ latex/slides/resources/01_grundlagen_1/helloworld.cs
@@ -3,12 +3,11 @@ namespace HelloWorld
 {
     class Hello
     {
-        static void Main()
+        public static void Main()
         {
             Console.WriteLine("Hello World!");
 
             // Laesst die Konsole offen.
-            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
     }
