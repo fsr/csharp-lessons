07a388b901188e4ecc130956d169bbe21d5192d7
Marc Satkowski <marc.satkowski@mail.de>
2016-09-29 01:29:45 +0200

    Update for lesson 9.

---
+++ latex/slides/resources/09_methoden_fortgeschritten_2/extension_using.cs
@@ -0,0 +1,5 @@
+string name = "Marc";
+// Hier steht nun: "I'm Marc. Hello Everyone!".
+string hallo = name.SayHelloFromMeTo("Everyone");
+// Die Anzahl der Woerter ist 4.
+int count = hallo.CountWords();
