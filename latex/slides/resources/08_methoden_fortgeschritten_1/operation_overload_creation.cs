69d6320e788e0ad89f5dba2a7d17ed268c5a98da
mushruptor <marcel.legler@gmx.de>
2016-11-10 11:44:13 +0100

    set 8 correction

--- latex/slides/resources/08_methoden_fortgeschritten_1/operation_overload_creation.cs
+++ latex/slides/resources/08_methoden_fortgeschritten_1/operation_overload_creation.cs
@@ -1,8 +1,11 @@
-public class EineKlasse() 
+public class EineKlasse
 {
+	public List<int> elements;
+	public EineKlasse(int anzahl) 
+	{ elements = new List<int>(anzahl);	}
     // Eine Ueberladung fuer + und true.
     public static EineKlasse operator +(EineKlasse a, EineKlasse b)
-    {   /*Code bzw. Berechnung*/   }    
+	{ return new EineKlasse(a.elements.Count + b.elements.Count); }
     public static bool operator true(EineKlasse obj)
-    {   /*Code bzw. Berechnung*/   }
+	{ return elements.Count > 0 ? true : false; }
 }
