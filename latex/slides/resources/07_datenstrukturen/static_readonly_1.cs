73bbc7713b983914c568707526c4101321111fbb
Marc Satkowski <marc.satkowski@mail.de>
2016-11-20 19:44:42 +0100

    Refinement of lesson 7

---
+++ latex/slides/resources/07_datenstrukturen/static_readonly_1.cs
@@ -0,0 +1,13 @@
+public class EineKlasse 
+{
+    // Statische Variable
+    public static int EineVariable;
+    // direkte Deklaration
+    private readonly string EinString = "Hallo";
+    private readonly string AndererString;
+    
+    public EineKlasse() 
+    {
+        AndererString = "Tschuess"; // Deklaration hier
+    }
+}
