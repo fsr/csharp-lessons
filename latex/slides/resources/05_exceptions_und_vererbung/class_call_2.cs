cd29b2d6b67ebc5a63e44aa811f8d9319fd93321
Marc Satkowski <marc.satkowski@mail.de>
2016-11-20 19:34:59 +0100

    Refinement of lesson 5

--- latex/slides/resources/05_exceptions_und_vererbung/class_call_2.cs
+++ latex/slides/resources/05_exceptions_und_vererbung/class_call_2.cs
@@ -1,15 +1,3 @@
-//Basisklasse
-public class Geld
-{
-	public void pay() { }
-}
-
-//Tochterklasse
-public class Dollar : Geld
-{
-	public void pay() {	}
-}
-
 //Aufruf
 Geld Banknote1 = new Geld()
 Geld Banknote2 = new Dollar()
